0f00a038c884b453066ac27cea60704846cb28ff
Lkasta <lkastabackup@gmail.com>
2025-07-20 20:05:28 -0300

    feat: create functions

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,12 @@
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  FlatList,
+  Alert,
+} from "react-native";
 
 import { Button } from "@/Components/Button";
 import { Input } from "@/Components/Input";
@@ -7,44 +15,118 @@ import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Filter } from "@/Components/Filters";
 import { Item } from "@/Components/Item";
+import { ItemStorage, itemsStorage } from "@/storage/itemsStorage";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING];
-const Items = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "1kg de Jonas",
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "Agora 2kg de Jonas skskksksksks",
-  },
-];
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+  const [items, setItems] = useState<ItemStorage[]>([]);
+
+  async function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert(
+        "Adicionar",
+        "Para adicionar um novo item adicione uma descrição"
+      );
+    }
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+
+    await itemsStorage.add(newItem);
+    await itemsByStatus();
+    setDescription("");
+    setFilter(FilterStatus.PENDING);
+  }
+
+  async function handleRemove(id: string) {
+    try {
+      await itemsStorage.remove(id);
+      await itemsByStatus();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleClear() {
+    Alert.alert("Limpar", "Você realmente deseja remover todos os itens da lista?", [
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => onClear() },
+    ]);
+  }
+
+  async function onClear() {
+    try {
+      await itemsStorage.clear();
+      setItems([]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function itemsByStatus() {
+    try {
+      const response = await itemsStorage.getByStatus(filter);
+      setItems(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleToggleItemStatus(id: string) {
+    try {
+      await itemsStorage.togleStatus(id);
+      await itemsByStatus();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    itemsByStatus();
+  }, [filter]);
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você quer comprar?" />
-        <Button title="Adicionar" />
+        <Input
+          placeholder="O que você quer comprar?"
+          onChangeText={setDescription}
+          value={description}
+        />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => {
-            return <Filter key={status} status={status} isActive />;
+            return (
+              <Filter
+                key={status}
+                status={status}
+                isActive={status === filter}
+                onPress={() => setFilter(status)}
+              />
+            );
           })}
 
-          <TouchableOpacity style={styles.clearButton} activeOpacity={0.8}>
+          <TouchableOpacity
+            style={styles.clearButton}
+            onPress={handleClear}
+            activeOpacity={0.8}
+          >
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
 
         <FlatList
-          data={Items}
+          data={items}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
@@ -52,8 +134,8 @@ export function Home() {
           renderItem={({ item }) => (
             <Item
               data={item}
-              onRemove={() => console.log("Remove Jonas")}
-              handleStatus={() => console.log("Muda Status")}
+              onRemove={() => handleRemove(item.id)}
+              handleStatus={() => handleToggleItemStatus(item.id)}
             />
           )}
           ListEmptyComponent={() => (
